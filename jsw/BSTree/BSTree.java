336b9503a1999b4accfb4ad4e62de28aac50dbad
Austin <asmacdo@gmail.com>
2014-04-09 15:40:11 -0400

    clean up child pointers

--- jsw/BSTree/BSTree.java
+++ jsw/BSTree/BSTree.java
@@ -15,41 +15,30 @@ public class BSTree<E extends Comparable<E>> {
         }
         
         Node curr = root;
-        int i;
         while (true) {
-            int cmp = newNode.compareTo(curr);
-            // newNode is less than curr
-            if (cmp == -1) {
-                i = 0; // links to left 
-            } else if (cmp == 1) {
-                i = 1; // links to right
-            } else { return false;} // must be 0, node is already there
-            if (curr.links[i]== null) {
-                curr.links[i] = newNode;
-                curr.links[i].height = curr.height + 1;
+            int cmp = 1 - newNode.compareTo(curr);
+            if (cmp == 1) {
+                return false;
+            }
+            if (curr.links[cmp]== null) {
+                curr.links[cmp] = newNode;
+                curr.links[cmp].height = curr.height + 1;
                 return true;
             } else {
-            curr = curr.links[i];
+                curr = curr.links[cmp];
             }
         }
     }
 
     public Node searchFor(E data) {
         Node curr = root;
-        int i;
         while (true) {
-            int cmp = data.compareTo((E)curr.data); 
-            if (cmp == -1) {
-                i = 0;  
-            } else if (cmp == 1) {
-                i = 1;
-            } else if (cmp == 0) {
-                return curr;
-            } else {
+            int cmp = data.compareTo((E)curr.data) + 1; 
+            if (cmp == 1) {
                 return null;
             }
-            if (curr.links[i] != null) {
-                curr = curr.links[i];
+            if (curr.links[cmp] != null) {
+                curr = curr.links[cmp];
             } else {
                 return null;
             }
@@ -67,7 +56,7 @@ public class BSTree<E extends Comparable<E>> {
         // pre order recursion
         String s = prettyPrint(curr.links[0]); // leftChild
         s += pre + curr + "\n";
-        s += prettyPrint(curr.links[1]); // right child
+        s += prettyPrint(curr.links[2]); // right child
         return s;
     }
 }
--- jsw/BSTree/Node.java
+++ jsw/BSTree/Node.java
@@ -1,7 +1,7 @@
 public class Node<E extends Comparable<E>> {
     E data;
     int height;
-    // array of links to other nodes. left, right, parent
+    // array of links to other nodes. left, parent, right
     Node[] links; 
     
     public Node(E data) {
